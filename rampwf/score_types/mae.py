4d844cb26faea494014849ad8ac775958172f964
Edouard Duchesnay <edouard.duchesnay@cea.fr>
2020-12-18 12:53:38 +0100

    MAE: Mean Absolute Error.

---
+++ rampwf/score_types/mae.py
@@ -0,0 +1,17 @@
+import numpy as np
+from .base import BaseScoreType
+
+
+class MAE(BaseScoreType):
+    """MAE: Mean Absolute Error."""
+
+    is_lower_the_better = True
+    minimum = 0.0
+    maximum = float('inf')
+
+    def __init__(self, name='mae', precision=2):
+        self.name = name
+        self.precision = precision
+
+    def __call__(self, y_true, y_pred):
+        return np.mean(np.abs(y_true - y_pred))
